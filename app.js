9c81dc0d36475617bd269125d29c965914e5e40c
Natália Fonseca <nataliabf@outlook.com>
2022-03-14 07:31:30 -0300

    feat: add product list in home page

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const vm = new Vue({
   el: "#app",
+  data: {
+    produtos: [],
+    notebook: {},
+    smartphone: {},
+    smartwatch: {},
+    speaker: {},
+    tablet: {},
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then((response) => response.json())
+        .then((json) => {
+          this.produtos = json;
+        });
+    },
+  },
+  created() {
+    this.fetchProdutos();
+  },
 });
